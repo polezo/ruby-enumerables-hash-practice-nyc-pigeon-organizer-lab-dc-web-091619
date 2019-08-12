eef3636de32c84be106c02b1e43499d43738c38c
Adam Finch <atyreefinch@gmail.com>
2019-08-12 14:56:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |key,value|
     value.each do |key2,pigeon_names|
-      
+      pigeon_names.each do |name|
     if pigeon_list[name]
       if pigeon_list[name][key]
       pigeon_list[name][key].push(key2)
