7627b3fd5bf7f367b11e55d95d0236eb04ffe50c
Adam Finch <atyreefinch@gmail.com>
2019-08-12 15:01:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,8 @@ def nyc_pigeon_organizer(data)
       pigeon_list[name] = {}
       pigeon_list[name][key] = []
       pigeon_list[name][key].push(key2).to_s
-    end
+      end
+     end
     end
   end
   pigeon_list
