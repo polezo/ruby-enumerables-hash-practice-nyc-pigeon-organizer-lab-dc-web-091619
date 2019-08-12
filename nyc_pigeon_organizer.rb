3e4de359a603b757b152bc77a02e167baf8aeb9c
Adam Finch <atyreefinch@gmail.com>
2019-08-12 15:21:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key,value|
+    value.each do |key2, pigeon_names|
+      pigeon_names.each do |name|
+        
+    unless pigeon_list[name] 
+      pigeon_list[name] = {}
+    end
+    
+    unless pigeon_list[name][key]
+        pigeon_list[name][key] = []
+     end   
+
+    pigeon_list[name][key].push(key2.to_s)
+    
+     end
+    end
+  end
+  pigeon_list
 end
+
